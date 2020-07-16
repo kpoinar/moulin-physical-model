db52049a1776c89643e457b5069769d9cd685acf
Celia Trunz <celia.trunz@gmail.com>
2020-07-16 16:32:17 -0400

    Update plot_celia.m

--- plot_celia.m
+++ plot_celia.m
@@ -1,3 +1,15 @@
-figure(10)
+clf
+figure(20)
+%plot(res{1,2}.t,res{1,2}.Qin)
+plot(res{1,2}.t,res{1,2}.Vadd_E)
+hold on
+plot(res{1,2}.t,res{1,2}.Vadd_C)
+plot(res{1,2}.t,res{1,2}.Vadd_turb)
 plot(res{1,2}.t,res{1,2}.Vadd_oc)
+%plot(res{1,2}.t,res{1,2}.Vadd_p)
+%plot(res{1,2}.t,res{1,2}.Qin_compensated)
+%legend('Qin','Qvadd')
+legend('Vadd E','Vadd C','Vadd turb','Vadd oc')%,'Vadd p')
+
+
 
