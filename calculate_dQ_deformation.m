0da6d34e1615d9e8c0bdc093119ad0fa258cf4db
Celia Trunz <30932911+cctrunz@users.noreply.github.com>
2020-07-23 11:16:51 -0400

    fix sign mistake calculate_dQ_deformation

--- calculate_dQ_deformation.m
+++ calculate_dQ_deformation.m
@@ -3,10 +3,10 @@ function dV = calculate_dQ_deformation(dMr_major,dMr_minor,M,z,wet)
 	%dMr_major: dE_major or dC_major
 	%dMr_minor: dC_minor or dC_minor
 
-	%New ellipse area - old ellipse area
-    dA_ellipse = pi .* (M.r_major+dMr_major) .* (M.r_minor+dMr_minor) - pi .* M.r_major .* M.r_minor;
-    %New circle area - old circle area
-    dA_circle = pi .* (M.r_minor+dMr_minor).^2 - pi .* M.r_minor.^2;
+	%Old ellipse area - New ellipse area
+    dA_ellipse = pi .* M.r_major .* M.r_minor - pi .* (M.r_major+dMr_major) .* (M.r_minor+dMr_minor);
+    %Old circle area - New circle area
+    dA_circle = pi .* M.r_minor.^2 - pi .* (M.r_minor+dMr_minor).^2;
     %total change in area calculated with half the ellipse and half the circle
     dA_tot = 0.5.*dA_ellipse + 0.5.*dA_circle;
     %change in volume below the water level for an entire timestep!
